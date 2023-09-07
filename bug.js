e45d845fedbf8cfa71cf3e348b632045d665a066
Andru1314 <144372034+Andru1314@users.noreply.github.com>
2023-09-07 19:52:12 +0300

    Add files via upload

---
+++ bug.js
@@ -0,0 +1,378 @@
+$(document).ready(function(){
+  $(".openNav").on('click', function () {
+    $(this).hide(1000);
+    $(".nav").slideDown(1000);
+    $(".closeNav").show(1000);
+  })
+  $(".closeNav").on('click', function () {
+    $(this).hide(1000);
+    $(".nav").slideUp(1000);
+    $(".openNav").show(1000);
+  })
+  
+
+// home---------------------------------------------------------------
+  $(".listoed").mouseenter(function () {
+    $(".listoedtxt").fadeIn(400)
+  }).mouseout(function () {
+    $(".listoedtxt").fadeOut(400)
+  })
+
+  $(".kongol").mouseenter(function () {
+    $(".kongoltxt").fadeIn(400)
+  }).mouseout(function () {
+    $(".kongoltxt").fadeOut(400)
+  })
+
+  $(".navoznik").mouseenter(function () {
+    $(".navozniktxt").fadeIn(400)
+  }).mouseout(function () {
+    $(".navozniktxt").fadeOut(400)
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+  $(".uk1").on('click', function(){
+    $("#uk1").fadeIn(500);
+    $(".uktxt1").fadeIn(500);
+    $("#uk2").fadeOut(500);
+    $("#uk3").fadeOut(500);
+    $("#uk4").fadeOut(500);
+    $("#uk5").fadeOut(500);
+    $("#uk6").fadeOut(500);
+    $("#uk7").fadeOut(500);
+    $("#uk8").fadeOut(500);
+    $("#uk9").fadeOut(500);
+    $("#uk10").fadeOut(500);
+
+
+
+    $(".uktxt2").fadeOut(500);
+    $(".uktxt3").fadeOut(500);
+    $(".uktxt4").fadeOut(500);
+    $(".uktxt5").fadeOut(500);
+    $(".uktxt6").fadeOut(500);
+    $(".uktxt7").fadeOut(500);
+    $(".uktxt8").fadeOut(500);
+    $(".uktxt9").fadeOut(500);
+    $(".uktxt10").fadeOut(500);
+  });
+
+
+
+
+  $(".uk2").on('click', function(){
+    $("#uk2").fadeIn(500);
+    $(".uktxt2").fadeIn(500);
+    $("#uk1").fadeOut(500);
+    $("#uk3").fadeOut(500);
+    $("#uk4").fadeOut(500);
+    $("#uk5").fadeOut(500);
+    $("#uk6").fadeOut(500);
+    $("#uk7").fadeOut(500);
+    $("#uk8").fadeOut(500);
+    $("#uk9").fadeOut(500);
+    $("#uk10").fadeOut(500);
+
+
+
+    $(".uktxt1").fadeOut(500);
+    $(".uktxt3").fadeOut(500);
+    $(".uktxt4").fadeOut(500);
+    $(".uktxt5").fadeOut(500);
+    $(".uktxt6").fadeOut(500);
+    $(".uktxt7").fadeOut(500);
+    $(".uktxt8").fadeOut(500);
+    $(".uktxt9").fadeOut(500);
+    $(".uktxt10").fadeOut(500);
+  });
+
+
+
+
+  $(".uk3").on('click', function(){
+    $("#uk3").fadeIn(500);
+    $(".uktxt3").fadeIn(500);
+    $("#uk1").fadeOut(500);
+    $("#uk2").fadeOut(500);
+    $("#uk4").fadeOut(500);
+    $("#uk5").fadeOut(500);
+    $("#uk6").fadeOut(500);
+    $("#uk7").fadeOut(500);
+    $("#uk8").fadeOut(500);
+    $("#uk9").fadeOut(500);
+    $("#uk10").fadeOut(500);
+
+
+
+
+    $(".uktxt1").fadeOut(500);
+    $(".uktxt2").fadeOut(500);
+    $(".uktxt4").fadeOut(500);
+    $(".uktxt5").fadeOut(500);
+    $(".uktxt6").fadeOut(500);
+    $(".uktxt7").fadeOut(500);
+    $(".uktxt8").fadeOut(500);
+    $(".uktxt9").fadeOut(500);
+    $(".uktxt10").fadeOut(500);
+  });
+
+
+
+
+
+  $(".uk4").on('click', function(){
+    $("#uk4").fadeIn(500);
+    $(".uktxt4").fadeIn(500);
+    $("#uk1").fadeOut(500);
+    $("#uk2").fadeOut(500);
+    $("#uk3").fadeOut(500);
+    $("#uk5").fadeOut(500);
+    $("#uk6").fadeOut(500);
+    $("#uk7").fadeOut(500);
+    $("#uk8").fadeOut(500);
+    $("#uk9").fadeOut(500);
+    $("#uk10").fadeOut(500);
+    
+
+
+
+    $(".uktxt1").fadeOut(500);
+    $(".uktxt2").fadeOut(500);
+    $(".uktxt3").fadeOut(500);
+    $(".uktxt5").fadeOut(500);
+    $(".uktxt6").fadeOut(500);
+    $(".uktxt7").fadeOut(500);
+    $(".uktxt8").fadeOut(500);
+    $(".uktxt9").fadeOut(500);
+    $(".uktxt10").fadeOut(500);
+  });
+
+
+
+
+
+  $(".uk5").on('click', function(){
+    $("#uk5").fadeIn(500);
+    $(".uktxt5").fadeIn(500);
+    $("#uk1").fadeOut(500);
+    $("#uk2").fadeOut(500);
+    $("#uk3").fadeOut(500);
+    $("#uk4").fadeOut(500);
+    $("#uk6").fadeOut(500);
+    $("#uk7").fadeOut(500);
+    $("#uk8").fadeOut(500);
+    $("#uk9").fadeOut(500);
+    $("#uk10").fadeOut(500);
+    
+
+
+    $(".uktxt1").fadeOut(500);
+    $(".uktxt2").fadeOut(500);
+    $(".uktxt3").fadeOut(500);
+    $(".uktxt4").fadeOut(500);
+    $(".uktxt6").fadeOut(500);
+    $(".uktxt7").fadeOut(500);
+    $(".uktxt8").fadeOut(500);
+    $(".uktxt9").fadeOut(500);
+    $(".uktxt10").fadeOut(500);
+  });
+
+
+
+
+  $(".uk6").on('click', function(){
+    $("#uk6").fadeIn(500);
+    $(".uktxt6").fadeIn(500);
+    $("#uk1").fadeOut(500);
+    $("#uk2").fadeOut(500);
+    $("#uk3").fadeOut(500);
+    $("#uk4").fadeOut(500);
+    $("#uk5").fadeOut(500);
+    $("#uk7").fadeOut(500);
+    $("#uk8").fadeOut(500);
+    $("#uk9").fadeOut(500);
+    $("#uk10").fadeOut(500);
+        
+
+
+    $(".uktxt1").fadeOut(500);
+    $(".uktxt2").fadeOut(500);
+    $(".uktxt3").fadeOut(500);
+    $(".uktxt4").fadeOut(500);
+    $(".uktxt5").fadeOut(500);
+    $(".uktxt7").fadeOut(500);
+    $(".uktxt8").fadeOut(500);
+    $(".uktxt9").fadeOut(500);
+    $(".uktxt10").fadeOut(500);
+  });
+
+
+
+
+  $(".uk7").on('click', function(){
+    $("#uk7").fadeIn(500);
+    $(".uktxt7").fadeIn(500);
+    $("#uk1").fadeOut(500);
+    $("#uk2").fadeOut(500);
+    $("#uk3").fadeOut(500);
+    $("#uk4").fadeOut(500);
+    $("#uk5").fadeOut(500);
+    $("#uk6").fadeOut(500);
+    $("#uk8").fadeOut(500);
+    $("#uk9").fadeOut(500);
+    $("#uk10").fadeOut(500);
+
+
+
+    $(".uktxt1").fadeOut(500);
+    $(".uktxt2").fadeOut(500);
+    $(".uktxt3").fadeOut(500);
+    $(".uktxt4").fadeOut(500);
+    $(".uktxt5").fadeOut(500);
+    $(".uktxt6").fadeOut(500);
+    $(".uktxt8").fadeOut(500);
+    $(".uktxt9").fadeOut(500);
+    $(".uktxt10").fadeOut(500);
+  });
+
+
+
+
+  $(".uk8").on('click', function(){
+    $("#uk8").fadeIn(500);
+    $(".uktxt8").fadeIn(500);
+    $("#uk1").fadeOut(500);
+    $("#uk2").fadeOut(500);
+    $("#uk3").fadeOut(500);
+    $("#uk4").fadeOut(500);
+    $("#uk5").fadeOut(500);
+    $("#uk6").fadeOut(500);
+    $("#uk7").fadeOut(500);
+    $("#uk9").fadeOut(500);
+    $("#uk10").fadeOut(500);
+    
+
+
+    $(".uktxt1").fadeOut(500);
+    $(".uktxt2").fadeOut(500);
+    $(".uktxt3").fadeOut(500);
+    $(".uktxt4").fadeOut(500);
+    $(".uktxt5").fadeOut(500);
+    $(".uktxt6").fadeOut(500);
+    $(".uktxt7").fadeOut(500);
+    $(".uktxt9").fadeOut(500);
+    $(".uktxt10").fadeOut(500);
+  });
+
+
+
+
+  $(".uk9").on('click', function(){
+    $("#uk9").fadeIn(500);
+    $(".uktxt9").fadeIn(500);
+    $("#uk1").fadeOut(500);
+    $("#uk2").fadeOut(500);
+    $("#uk3").fadeOut(500);
+    $("#uk4").fadeOut(500);
+    $("#uk5").fadeOut(500);
+    $("#uk6").fadeOut(500);
+    $("#uk7").fadeOut(500);
+    $("#uk8").fadeOut(500);
+    $("#uk10").fadeOut(500);
+        
+
+
+    $(".uktxt1").fadeOut(500);
+    $(".uktxt2").fadeOut(500);
+    $(".uktxt3").fadeOut(500);
+    $(".uktxt4").fadeOut(500);
+    $(".uktxt5").fadeOut(500);
+    $(".uktxt6").fadeOut(500);
+    $(".uktxt7").fadeOut(500);
+    $(".uktxt8").fadeOut(500);
+    $(".uktxt10").fadeOut(500);
+  });
+
+
+
+  
+  $(".uk10").on('click', function(){
+    $("#uk10").fadeIn(500);
+    $(".uktxt10").fadeIn(500);
+    $("#uk1").fadeOut(500);
+    $("#uk2").fadeOut(500);
+    $("#uk3").fadeOut(500);
+    $("#uk4").fadeOut(500);
+    $("#uk5").fadeOut(500);
+    $("#uk6").fadeOut(500);
+    $("#uk7").fadeOut(500);
+    $("#uk8").fadeOut(500);
+    $("#uk9").fadeOut(500);
+            
+
+
+    $(".uktxt1").fadeOut(500);
+    $(".uktxt2").fadeOut(500);
+    $(".uktxt3").fadeOut(500);
+    $(".uktxt4").fadeOut(500);
+    $(".uktxt5").fadeOut(500);
+    $(".uktxt6").fadeOut(500);
+    $(".uktxt7").fadeOut(500);
+    $(".uktxt8").fadeOut(500);
+    $(".uktxt9").fadeOut(500);
+  });
+
+
+
+
+
+  $(".bug1").mouseenter(function () {
+    $(".point1").fadeIn(400)
+  }).mouseout(function () {
+    $(".point1").fadeOut(400)
+  })
+
+
+  $(".bug2").mouseenter(function () {
+    $(".point2").fadeIn(400)
+  }).mouseout(function () {
+    $(".point2").fadeOut(400)
+  })
+
+  $(".bug3").mouseenter(function () {
+    $(".point3").fadeIn(400)
+  }).mouseout(function () {
+    $(".point3").fadeOut(400)
+  })
+
+  $(".bug4").mouseenter(function () {
+    $(".point4").fadeIn(400)
+  }).mouseout(function () {
+    $(".point4").fadeOut(400)
+  })
+
+  $(".bug5").mouseenter(function () {
+    $(".point5").fadeIn(400)
+  }).mouseout(function () {
+    $(".point5").fadeOut(400)
+  })
+
+  $(".bug6").mouseenter(function () {
+    $(".point6").fadeIn(400)
+  }).mouseout(function () {
+    $(".point6").fadeOut(400)
+  })
+
+
+})
